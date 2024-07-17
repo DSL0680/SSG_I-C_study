2a76fd020ffe7346d81b9b284842350e3bc6316a
DSL0680 <ekthf988172@gmail.com>
2024-07-17 17:27:20 +0900

    day3

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,50 @@
+package org.example;
+
+
+import lombok.Data;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@Data
+
+public class Main {
+    public static void main(String[] args) {
+
+        PiggySaveUI piggySaveUI = new PiggySaveUI();
+        piggySaveUI.showMenus();
+
+
+
+
+
+
+
+
+
+
+
+//        Player player = new Player(80, "King Author");
+//
+//        Monster monster = new Monster(30);
+//
+//        for (int i = 0; i < 3; i++) {
+//            int effect = player.attack();
+//            System.out.println("Player attacked: " + effect);
+//            System.out.println("MONSTER: " + monster.damage(effect));
+//
+//            if (monster.hp <= 0) {
+//                System.out.println("Monster dead! You Win!");
+//                break;
+//            }
+//
+//            int monsterEffect = monster.attack();
+//            System.out.println("Monster attacked: " + monsterEffect);
+//            System.out.println("PLAYER: " + player.defense(monsterEffect));
+
+//        PiggySave myPiggySave = new PiggySave();
+//        PiggySave yourPiggySave = new PiggySave();
+
+
+//        }
+    }
+}
