a367c7465ac5676a64d096ba6358d41d7b6e20f5
DSL0680 <ekthf988172@gmail.com>
2024-08-20 09:26:54 +0900

    컴포넌트분리

---
+++ src/pages/SamplePage.vue
@@ -0,0 +1,28 @@
+<template>
+<MyNav></MyNav>
+<div class="container">
+    <MyTitle></MyTitle>
+
+    <Counter2></Counter2>
+
+    <hr>
+    
+    <MyFooter></MyFooter>
+</div>
+</template>
+
+<script setup>
+import Counter1 from '../components/ex1/Counter1.vue';
+import Counter2 from '../components/ex1/Counter2.vue';
+import Ex1 from '../components/ex1/Ex1.vue';
+import MyContent from '../components/MyContent.vue';
+import MyFooter from '../components/MyFooter.vue';
+import MyNav from '../components/MyNav.vue';
+import MyTitle from '../components/MyTitle.vue';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
