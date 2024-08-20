e26a19add2ae22f14437abf6a30fb5e93f3fdd22
DSL0680 <ekthf988172@gmail.com>
2024-08-20 16:20:48 +0900

    Todo List

--- src/components/ex4/TodoBox.vue
+++ src/components/ex4/TodoBox.vue
@@ -13,9 +13,9 @@ const props = defineProps(['title'])
 <style scoped>
 div {
     border: 1px solid black;
-    width: 100vw;
+    width: 50vw;
     min-height: 10vh;
     margin: 0.2em;
-    padding: 1em;
+    padding: 0.3em;    
 }
 </style>
