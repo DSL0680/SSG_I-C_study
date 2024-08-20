60f886cba1c8052fb1b4992e2516a0286db610c6
DSL0680 <ekthf988172@gmail.com>
2024-08-20 17:55:06 +0900

    Todo List v2

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import SamplePage from './pages/SamplePage.vue';
+</script>
+
+<template>
+<SamplePage></SamplePage>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
